630aea343f4a445ca5784869954a8544267bb90f
iHavebraincells <rizzbash32@gmail.com>
2025-06-20 23:18:06 -0400

    Update uv.sw.js

--- static/uv/uv.sw.js
+++ static/uv/uv.sw.js
@@ -1,6 +1,1 @@
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).catch(() => new Response('You are offline.'))
-  );
-});
-
+self.addEventListener('fetch', () => {});
