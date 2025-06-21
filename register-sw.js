399f9c1011a334aa9e9d1faffb9ff0a7ffd76bf3
iHavebraincells <rizzbash32@gmail.com>
2025-06-20 23:00:06 -0400

    Update register-sw.js

--- register-sw.js
+++ register-sw.js
@@ -1,7 +1,3 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('static/uv/uv.sw.js')
-      .then(reg => console.log('Service Worker registered!', reg))
-      .catch(err => console.error('Service Worker registration failed:', err));
-  });
-}
+navigator.serviceWorker.register("./static/uv/uv.sw.js", {
+  scope: __uv$config.prefix
+});
