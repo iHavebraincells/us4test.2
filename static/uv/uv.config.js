83242fa6a5bef29501eac9b59c7b99fd4fd3de2b
iHavebraincells <rizzbash32@gmail.com>
2025-06-20 23:51:36 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,5 +1,5 @@
-self.__uv$config = {
-  prefix: '/service',
+window.__uv$config = {
+  prefix: '/service/',
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
 };
