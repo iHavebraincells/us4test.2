695e79f722575c9d33e63547bd1b37077899fab3
iHavebraincells <rizzbash32@gmail.com>
2025-06-20 23:51:06 -0400

    Update register-sw.js

--- register-sw.js
+++ register-sw.js
@@ -1,3 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('static/uv/uv.sw.js', { scope: '/service/' });
+  navigator.serviceWorker.register('/static/uv/uv.sw.js', {
+    scope: '/service/',
+  });
 }
