fa248a4b99cbffc2fcab2517c8b162dc59a65667
iHavebraincells <rizzbash32@gmail.com>
2025-06-21 00:09:25 -0400

    Update uv.bundle.js

--- static/uv/uv.bundle.js
+++ static/uv/uv.bundle.js
@@ -2,7 +2,9 @@ window.Ultraviolet = {
   codec: {
     xor: {
       encode: function (str) {
-        return btoa([...str].map((c, i) => i % 2 ? String.fromCharCode(c.charCodeAt() ^ 2) : c).join(''));
+        return btoa([...str].map((c,i) =>
+          i % 2 ? String.fromCharCode(c.charCodeAt() ^ 2) : c
+        ).join(''));
       },
       decode: function (b64) {
         return atob(b64);
