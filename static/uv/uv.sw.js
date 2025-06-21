c0be475ddbcec8ca5625504dafb3463af22a789b
iHavebraincells <rizzbash32@gmail.com>
2025-06-20 23:45:35 -0400

    Update uv.sw.js

--- static/uv/uv.sw.js
+++ static/uv/uv.sw.js
@@ -1,14 +1,4 @@
-self.__uv$config = {
-  prefix: '/service/',
-  bare: 'https://uv.bypass.workers.dev/',
-  encodeUrl: function (url) {
-    return new URL(self.__uv$config.prefix + btoa(url).replace(/=/g, ''), location).toString();
-  },
-};
-
-self.addEventListener('fetch', event => {
-  const url = new URL(event.request.url);
-  if (!url.pathname.startsWith(self.__uv$config.prefix)) return;
-  const target = atob(url.pathname.slice(self.__uv$config.prefix.length));
-  event.respondWith(fetch(new Request(target, event.request)));
-});
+importScripts('./uv.bundle.js');
+importScripts('./uv.config.js');
+const sw = new UVServiceWorker();
+self.addEventListener('fetch', e => e.respondWith(sw.fetch(e)));
